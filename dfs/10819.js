3379eb10cde50e5f3a041f0293433f3edba45d2a
hyokyong <hyokyong76199@hanmail.net>
2024-06-18 21:09:28 +0900

    10819

---
+++ dfs/10819.js
@@ -0,0 +1,8 @@
+let fs = require("fs");
+let path = require("path");
+
+let filePath = path.join(__dirname, "example.txt");
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+let num = Number(input[0]);
+let arr = input[1].map(Number);
