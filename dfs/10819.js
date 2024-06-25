0053c53686c47bed78640d94077ce5e2f35c0594
hyokyong <hyokyong76199@hanmail.net>
2024-06-25 20:18:05 +0900

    10819

--- dfs/10819.js
+++ dfs/10819.js
@@ -4,5 +4,23 @@ let path = require("path");
 let filePath = path.join(__dirname, "example.txt");
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let num = Number(input[0]);
-let arr = input[1].map(Number);
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+function dsf(graph, v, visited) {
+  console.log("v", v);
+  visited[v] = true;
+  console.log(v);
+
+  for (let i of graph[v]) {
+    if (!visited[i]) {
+      dsf(graph, i, visited);
+    }
+  }
+}
+
+const graph = [[], [2, 3, 4], [1], [1, 5, 6], [1, 7], [3, 8], [3], [4], [5]];
+
+const visited = new Array(9).fill(false);
+
+dsf(graph, 1, visited);
